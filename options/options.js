78e360fdf4504ce06f82a9b3baf446adeca90d7f
Crowdbotics <team@crowdbotics.com>
2022-06-02 19:11:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mindful_magic_35570",
+  url: "https://mindful_magic_35570.botics.co",
+  api: "https://mindful_magic_35570.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
